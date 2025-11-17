c45ad0fe97325c2ffb161fb9a1e0b9406061c061
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2025-11-17 13:02:28 +0100

    Interface Segregation

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,30 @@
+interface Bird {
+    fly(): void;
+    eat(): void;
+    run(): void;
+}
+
+class Sparrow implements Bird {
+    public fly(){}
+    public eat(){}
+    public run(){}
+}
+
+class Tucan implements Bird {
+    public fly(){}
+    public eat(){}
+    public run(){}
+}
+
+class Ostrich implements Bird {
+    public fly(){}
+    public eat(){}
+    public run(){}
+}
+
+class Penguin implements Bird {
+    public fly(){}
+    public eat(){}
+    public run(){}
+}
+
